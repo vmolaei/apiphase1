eddcab3a3138b7542bd6a66c1bdbb0c90bf2b403
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-12 21:19:40 +0330

    Laravel api without authentication

--- app/Http/Controllers/Api/Apicontroller.php
+++ app/Http/Controllers/Api/Apicontroller.php
@@ -38,19 +38,79 @@ class Apicontroller extends Controller
     }
     //List API  --get
     public function listEmployees(){
+        $employees = Employee::get();
+        return response()->json([
+            "status"=>1,
+            "message"=>"List emoloyees",
+            "data"=>$employees
+        ],200);
 
     }
     //Single Detail API   --get
     public function getSingleEmployee($id){
+        if(Employee::where("id",$id)->exists()){
+            $employee_detail = Employee::where("id",$id)->first();
+            return  response([
+                "status"=>1,
+                "messahe"=>"Employee found",
+                "data"=>$employee_detail
+            ]);
+
+        }else{
+            return response([
+                "status"=>0,
+                "message"=>"Employee not found"
+            ],404);
+        }
 
 
     }
     //Update Employee  --put
     public function udateEmployee(Request $request,$id){
+        if(Employee::where("id",$id)->exists()){
+            $employee = Employee::find($id);
+
+            $employee->name = !empty($request->name)?$request->name : $employee->name;
+            $employee->email = !empty($request->email)?$request->email : $employee->email;
+            $employee->phone_no = !empty($request->phone_no)? $request->phone_no : $employee->phone_no;
+            $employee->gender = !empty($request->gender)? $request->gender : $employee->gender;
+            $employee->age = !empty($request->age)?$request->age : $employee->age;
+
+            $employee->save();
+
+            return response([
+                "status"=>1,
+                "message"=>"Employee updated successfully"
+
+            ]);
+
+
+        }else{
+            return response([
+                "status"=>0,
+                "message"=>"Employee not found"
+            ],404);
+
+        }
 
     }
     //delete API   --delete
     public function deleteEmployee($id){
+        if(Employee::where("id",$id)->exists()){
+            $employee=Employee::find($id);
+            $employee->delete();
+            return response()->json([
+                "status"=>1,
+                "message"=>"Employee delete successfully"
+            ]);
+
+        }else{
+            return response([
+                "status"=>0,
+                "message"=>"Employee not found"
+            ],404);
+
+        }
 
     }
 }
